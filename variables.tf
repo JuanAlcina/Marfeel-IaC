eaf6462e951d4cef922a5433961b54b15b9db833
Sebastian Alcina <sebastian_alcina@epam.com>
2023-03-07 18:15:53 -0500

    Added configuration for vpc and eks cluster

--- variables.tf
+++ variables.tf
@@ -1,7 +1,21 @@
 variable region {
     type = string
+    description = "AWS region used to deploy resources"
 }
 
 variable profile {
     type = string
+    description = "Profile used to deploy resources"
+}
+
+variable vpc_name {
+    type = string
+    default = "Marfeel-VPC"
+    description = "Name of AWS vpc"
+}
+
+variable cluster_name {
+    type = string
+    default = "Marfeel-EKS"
+    description = "Name of the EKS cluster"
 }
